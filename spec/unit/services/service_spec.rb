e8ff1825116738245f242e8c981232763db1ebc0
Ed Robinson <ed.robinson@reevoo.com>
2015-03-31 21:08:43 +0100

    Add service spec

---
+++ spec/unit/services/service_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+require 'services/service'
+
+describe Services::Service do
+  subject { described_class.new(application: nil, data: nil) }
+  describe '#env' do
+    it 'defaults to an empty hash' do
+      expect(subject.env).to eq({})
+    end
+  end
+end
