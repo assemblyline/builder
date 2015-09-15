74e128cb67b71f9be9898e2dc5edd81a1de1c125
Ed Robinson <ed.robinson@reevoo.com>
2015-09-15 15:36:38 +0100

    RabbitMQ Service

---
+++ lib/services/rabbitmq.rb
@@ -0,0 +1,17 @@
+require 'services/service'
+require 'colorize'
+require 'log'
+
+module Services
+  class RabbitMQ < Service
+    def env
+      { 'AMQP_URI' => amqp_uri }
+    end
+
+    private
+
+    def amqp_uri
+      "amqp://guest:guest@#{ip}/"
+    end
+  end
+end
