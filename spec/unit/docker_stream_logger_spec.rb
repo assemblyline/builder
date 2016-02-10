6d101a7e52f8a44d17ab5f19ba1e13540c935464
Ed Robinson <ed@reevoo.com>
2016-02-10 16:38:15 +0000

    Fix failing specs

--- spec/unit/docker_stream_logger_spec.rb
+++ spec/unit/docker_stream_logger_spec.rb
@@ -9,7 +9,7 @@ describe DockerStreamLogger do
     before { stream_fixture(DockerStreamFixtures::IMAGE_PULL) }
 
     it "Prints a message before pulling the image" do
-      expect(output.first).to eq "Pulling from assemblyline/builder-frontendjs:0.12.0"
+      expect(output.first).to eq "Pulling from assemblyline/builder-frontendjs:0.12.0".bold.green
     end
 
     it "formats a progress bar when downloading an image" do
@@ -17,7 +17,7 @@ describe DockerStreamLogger do
     end
 
     it "Prints a message when the download is done" do
-      expect(output.last).to eq "Status: Downloaded newer image for quay.io/assemblyline/builder-frontendjs:0.12.0"
+      expect(output.last).to eq "Status: Downloaded newer image for quay.io/assemblyline/builder-frontendjs:0.12.0".bold.green
     end
   end
 
@@ -30,9 +30,9 @@ describe DockerStreamLogger do
     end
 
     it "formats the status info correctly" do
-      expect(output[1]).to eq "Pulling from assemblyline/ubuntu:14.04.2"
-      expect(output[2]).to eq "Digest: sha256:5ae07f4ea4e0adf0f86e9d703e2e783d1bbf4d76aaf06092fb1bce3e625ad7bf"
-      expect(output[3]).to eq "Status: Image is up to date for quay.io/assemblyline/ubuntu:14.04.2"
+      expect(output[1]).to eq "Pulling from assemblyline/ubuntu:14.04.2".bold.green
+      expect(output[2]).to eq "Digest: sha256:5ae07f4ea4e0adf0f86e9d703e2e783d1bbf4d76aaf06092fb1bce3e625ad7bf".bold.green
+      expect(output[3]).to eq "Status: Image is up to date for quay.io/assemblyline/ubuntu:14.04.2".bold.green
     end
   end
 
@@ -45,10 +45,10 @@ describe DockerStreamLogger do
     end
 
     it "formats a progress bar when downloading the image" do
-      expect(output[1]).to eq "Pulling from assemblyline/ubuntu:14.04.2"
+      expect(output[1]).to eq "Pulling from assemblyline/ubuntu:14.04.2".bold.green
       expect(output[4]).to eq "Downloading Image: |===========================================================|"
-      expect(output[5]).to eq "Digest: sha256:5ae07f4ea4e0adf0f86e9d703e2e783d1bbf4d76aaf06092fb1bce3e625ad7bf"
-      expect(output[6]).to eq "Status: Downloaded newer image for quay.io/assemblyline/ubuntu:14.04.2"
+      expect(output[5]).to eq "Digest: sha256:5ae07f4ea4e0adf0f86e9d703e2e783d1bbf4d76aaf06092fb1bce3e625ad7bf".bold.green
+      expect(output[6]).to eq "Status: Downloaded newer image for quay.io/assemblyline/ubuntu:14.04.2".bold.green
     end
   end
 
